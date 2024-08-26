7e0843ffde9ac4c464a2404fe23ba52ef3989d1e
Marten O'Grady <35665595+Escape613@users.noreply.github.com>
2024-08-26 19:58:27 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello from <a href="https://www.laconic.com/">Laconic!</a>
+          HELLO FROM <a href="https://www.laconic.com/">LACONIC!</a>
         </h1>
 
         <div className={styles.grid}>
